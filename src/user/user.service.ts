992ddea4195736c0397d10f9d168d70e2a1cd2c7
sandromatiass <devsandromatias@gmail.com>
2023-07-07 22:39:29 -0300

    criado entitys de estado e populando os seus dados

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
-import { UserEntity } from './interfaces/user.entity';
+import { UserEntity } from './entities/user.entity';
 import { hash } from 'bcrypt';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
