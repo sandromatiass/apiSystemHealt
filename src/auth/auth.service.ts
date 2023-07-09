28b650d2deeffb94191934d093d2221361c64991
sandromatiass <devsandromatias@gmail.com>
2023-07-09 19:52:20 -0300

    feat: create jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,28 +3,39 @@ import { LoginDto } from './dtos/login.dto';
 import { UserEntity } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
 import { compare } from 'bcrypt';
+import { ReturnLogin } from './dtos/returnLogin.dto';
+import { LoginPayload } from './dtos/loginPayload.dto';
+import { JwtService } from '@nestjs/jwt';
+import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
 
 @Injectable()
 export class AuthService {
   
   constructor(
-    private readonly userService: UserService
+    private readonly userService: UserService, 
+    private jwtService: JwtService,
   ) {}
      
-//aqui vamos ter que ver como fazer a validação primeiro por cpf, cnpj, matricula no caso do 
-//coloborado dps a gente ver o lado do paciente
-//colocar os requerimento como unico aqueles de autenticação
-
-  async login(loginDto: LoginDto): Promise<UserEntity>{
-    const user: UserEntity | undefined = await this.userService.findUserByCnpj(loginDto.cnpj).catch(() => undefined)
+  async login(loginDto: LoginDto): Promise<ReturnLogin>{
+    
+    const user: UserEntity | undefined = await this.userService
+    .findUserByCnpj(loginDto.cnpj)
+    .catch(() => undefined)
 
     const isMatch = await compare(loginDto.password, user?.password || '');
-
 
     if(!user || !isMatch){
       throw new NotFoundException(`CNPJ or password invalid!`)
     }
 
-    return user;
+    return {
+      accessToken: this.jwtService.sign({ ...new LoginPayload(user)}),
+      user: new ReturnUserDto(user),
+    }
   }
 }
+
+
+//aqui vamos ter que ver como fazer a validação primeiro por cpf, cnpj, matricula no caso do 
+//coloborado dps a gente ver o lado do paciente
+//colocar os requerimento como unico aqueles de autenticação
